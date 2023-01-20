313dd0a76a0e74eb4669858e35fe166f6b949d9e
anthony <anthonytguilherme@hotmail.com>
2023-01-20 16:22:18 -0300

    add styled component and logo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "./components/LogoAnime";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Logo/>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/LogoAnime/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import logo from '../../assets/imgs/Logo.png'
+
+const Logo = styled.img.attrs({ src: logo, alt: 'Logo do projeto' })`
+    height: 40px;
+`;
+
+export default Logo;
