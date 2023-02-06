cff2d62f3dbde0de4101702617f164598e13070c
anthony <anthonytguilherme@hotmail.com>
2023-02-06 13:19:09 -0300

    created a dump function for the arrow in css

--- src/components/CarrouselAnimeList/index.jsx
+++ src/components/CarrouselAnimeList/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CarrouselStyle, Right } from './styles';
+import { CarrouselStyle, Right, Wrapper } from './styles';
 import ThumbAnime from '../ThumbAnime';
 
 function CarrouselAnimeList({ videos }) {
   return (
     <CarrouselStyle>
-      {videos.map(({ src, alt, avatar, animeName, link }) => (
-        <ThumbAnime
-          src={src}
-          alt={alt}
-          avatar={avatar}
-          animeName={animeName}
-          link={link}
-        />
-      ))}
+      <Wrapper>
+        {videos.map(({ src, alt, avatar, animeName, link }) => (
+          <ThumbAnime
+            src={src}
+            alt={alt}
+            avatar={avatar}
+            animeName={animeName}
+            link={link}
+          />
+        ))}
+      </Wrapper>
       <Right />
     </CarrouselStyle>
   );
--- src/components/CarrouselAnimeList/styles.js
+++ src/components/CarrouselAnimeList/styles.js
@@ -1,16 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Background, WrapperThumb } from '../ThumbAnime/styles';
 import arrow from '../../assets/imgs/arrow.svg';
+
+export const Wrapper = styled.div`
+  display: flex;
+
+  & > ${Background} {
+    margin-right: 12rem;
+  }
+  ${({ moveRight }) =>
+    moveRight &&
+    css`
+      transform: translateX(calc(var(--thumb - width) * -1));
+    `}
+`;
 
 export const Right = styled.button`
   position: absolute;
   width: 50rem;
-  height: calc(100% - var(--space-top-bottom) * 2);
+  height: calc(100 % - var(--space - top - bottom) * 2);
   right: 0;
   opacity: 0;
   border: none;
   cursor: pointer;
-  background-color: var(--color-black-medium);
+  background-color: var(--color - black - medium);
   transition: opacity 200ms linear;
 
   &::after {
@@ -20,20 +33,17 @@ export const Right = styled.button`
 
 export const CarrouselStyle = styled.div`
   --space-top-bottom: 20rem;
+  --thumb-width: 296.5px;
   position: relative;
   display: flex;
-  align-self: flex-start;
+  align-self: flex - start;
   width: 100%;
-  box-sizing: border-box;
-  padding: var(--space-top-bottom) 30rem;
+  box-sizing: border - box;
+  padding: var(--space - top - bottom) 30rem;
   overflow: hidden;
 
-  & > ${Background} {
-    margin-right: 12rem;
-  }
-
   & ${WrapperThumb} {
-    width: 296.5px;
+    width: var(--thumb - width);
   }
 
   &:hover > ${Right} {
